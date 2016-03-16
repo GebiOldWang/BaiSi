5fee9cb1be1db3ba48fa2a97cafb7771472b9837
GebiOldWang <Email840226514@qq.com>
2016-03-16 10:38:14 +0800

    配置git

---
+++ 仿-百思不得姐/仿-百思不得姐/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  仿-百思不得姐
+//
+//  Created by 上海银来（集团）有限公司 on 16/3/16.
+//  Copyright © 2016年 Old.Wang. All rights reserved.
+//  Action
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
